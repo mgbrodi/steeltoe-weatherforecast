72ada4d6139b981c00cb14616c2f5f90ca10afeb
Maria Gabriella Brodi <mbrodi@pivotal.io>
2022-03-18 13:14:26 -0400

    more substitutions

--- Program.cs
+++ Program.cs
@@ -3,7 +3,7 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Hosting;
 using Steeltoe.Extensions.Logging;
 
-namespace newyorkWeather
+namespace Sample
 {
     public class Program
     {
